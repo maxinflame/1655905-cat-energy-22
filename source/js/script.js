0c1457dc3fd089ee89aee4fc952b82ab33289fb2
maxinflame <maxinflame@yandex.ru>
2021-05-27 18:06:08 +0300

    build

--- source/js/script.js
+++
@@ -1,9 +0,0 @@
-var mainNav = document.querySelector('.main-nav');
-var openButton = mainNav.querySelector('.main-nav__open-button');
-
-mainNav.classList.remove('main-nav--no-js');
-
-openButton.addEventListener('click', function(evt) {
-  mainNav.classList.toggle('main-nav--opened');
-  mainNav.classList.toggle('main-nav--closed');
-})
